e76390bca8316e0eb229e2f4e4b6fb987bf4fd23
hackmao <152413068+hackmao@users.noreply.github.com>
2023-12-16 22:41:26 +0800

    Add files via upload

---
+++ html/1.js
@@ -0,0 +1,18 @@
+
+const fs = require('fs');
+var minify = require('html-minifier').minify;
+const dir = './html/';
+
+var html = "";
+html += fs.readFileSync('./p/head.html')
+var l = fs.readdirSync(dir)
+for (let i = 0; i < l.length; i++) {
+    html += fs.readFileSync(dir + l[i])
+}
+html += fs.readFileSync('./p/foot.html')
+html = minify(html, {
+    removeComments: true, collapseWhitespace: true, minifyJS: true, minifyCSS: true
+})
+fs.writeFileSync('../m.html', html)
+html = fs.readFileSync('./p/h.html') + html
+fs.writeFileSync('../index.php', html)
